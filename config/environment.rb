4cb4d5b8d4b2ff0cb74d2ac6b49687d0c579ace5
mysta3 <myshawnestallings@gmail.com>
2021-01-25 16:54:50 -0800

    refactored code into partials

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Removes field with error settings
+ActionView::Base.field_error_pro = Proc.new do |html_tag, instance| 
+  html_tag.html_safe 
+end
