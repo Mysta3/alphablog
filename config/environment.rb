16cbb2f2a0cade616c0caa6568ea2becb0ac4de7
mysta3 <myshawnestallings@gmail.com>
2021-01-27 09:15:35 -0800

    added bootstrap

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,6 @@ require_relative "application"
 Rails.application.initialize!
 
 # Removes field with error settings
-ActionView::Base.field_error_pro = Proc.new do |html_tag, instance| 
-  html_tag.html_safe 
-end
+# ActionView::Base.field_error_pro = Proc.new do |html_tag, instance| 
+#   html_tag.html_safe 
+# end
