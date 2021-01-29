d3d89a878f24f3785f62221a365779c968a0ed1b
mysta3 <myshawnestallings@gmail.com>
2021-01-28 16:46:04 -0800

    fixed users table and added validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_000503) do
+ActiveRecord::Schema.define(version: 2021_01_29_003106) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
