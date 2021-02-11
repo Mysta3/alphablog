6fecf920fe30e205b404f75627386d0a88d24c53
mysta3 <myshawnestallings@gmail.com>
2021-02-10 16:57:21 -0800

    added logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
+    
     @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
@@ -13,9 +13,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create 
+  def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else 
@@ -24,11 +25,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
 
+ 
+
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       flash[:notice] = "Profile was updated successfully."
       redirect_to @user
@@ -41,5 +44,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
