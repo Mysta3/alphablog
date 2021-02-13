5b8145cb185fb18017c781e25336a2bcd4259e80
mysta3 <myshawnestallings@gmail.com>
2021-02-12 16:03:01 -0800

    aded delete profile function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, except: [:show, :update]
   before_action :require_same_user, only: [:edit, :update]
 
@@ -43,6 +43,15 @@ class UsersController < ApplicationController
     end
   end
 
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+
+  
   private
 
   def user_params
