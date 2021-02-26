af9e5463190e22960395a0ce1e9c1c5defbecbc3
mysta3 <myshawnestallings@gmail.com>
2021-02-25 17:14:49 -0800

    added tests for Category model

---
+++ test/models/categories_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+
+  def setup
+    ## anything written here is run before each test is run.
+    @category = Category.new(name: "Tech")
+  end
+
+  test 'category should be valid' do
+    assert @category.valid?
+  end
+
+  test 'name should be present' do
+    @category.name = ' '
+    assert_not @category.valid?
+  end
+
+  test 'name should be unique' do
+    @category.save
+    @category2 = Category.new(name: 'Tech')
+    assert_not @category2.valid?
+  end
+
+  test 'name should not be too long' do
+    @category.name = 'a' * 26
+    assert_not @category.valid?
+  end
+
+  test 'name should not be too short' do
+    @category.name = 'aa'
+    assert_not @category.valid?
+  end
+end
