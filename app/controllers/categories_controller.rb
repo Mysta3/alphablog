9c3bcdebb8341dcd62a292c0bb9186f3c0e12157
mysta3 <myshawnestallings@gmail.com>
2021-03-06 15:22:55 -0800

    added test for create action, new form creation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,10 +5,28 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = 'Category was successfully created'
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def show
 
+  end
+
+  private
+
+  def category_params
+    # whitelisting params 
+    params.require(:category).permit(:name)
   end
 end
