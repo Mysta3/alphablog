236ee2e07edd6e9d1f229ed3fd8982090bed05ad
mysta3 <myshawnestallings@gmail.com>
2021-03-06 15:51:00 -0800

    added invalid submission testing

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -12,4 +12,17 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success 
     assert_match "Augemented Reality", response.body
   end
+
+  # TEST FOR INVALID SUBMISSIONS
+  test "get new category form and reject invalid category submission" do
+    # workflow of tests
+    get "/categories/new"
+    assert_response :success 
+    assert_no_difference 'Category.count' do 
+      post categories_path, params: { category: { name: " " }}
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
 end
