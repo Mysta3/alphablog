1b2c9d052c22938cfa8ecae16c74762615596e84
mysta3 <myshawnestallings@gmail.com>
2021-03-06 14:41:08 -0800

    Added tests for Categories, built new,show,index pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  # Article Routes
   resources :articles # -> gives all routes for articles resource
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  # Category Routes
+  resources :categories, except: [:destroy]
 end
